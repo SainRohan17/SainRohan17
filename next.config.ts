bc64ca0d106859919d7a549adf8908d68a51ceb4
Rohan Sain <rohan170543@gmail.com>
2026-07-10 19:03:13 +0530

    Configure Next.js with output and basePath settings

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/SainRohan17",
+  assetPrefix: "/SainRohan17/",
+};
 
 export default nextConfig;
